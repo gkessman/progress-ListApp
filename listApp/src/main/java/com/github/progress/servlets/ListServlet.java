4115d6a33153efd4fac0b36b2b5344a574e27e2e
John <John.Schintone@gmail.com>
2015-08-17 20:45:52 -0400

    init

---
+++ listApp/src/main/java/com/github/progress/servlets/ListServlet.java
@@ -0,0 +1,35 @@
+package com.github.progress.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListServlet extends HttpServlet {
+
+	public ListServlet() {
+			
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("did a get");
+		super.doGet(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPut(req, resp);
+	}
+
+}
+
+
